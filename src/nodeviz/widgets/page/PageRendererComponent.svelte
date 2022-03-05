e6c379f9b0c8c55e95e88df2641a4331fd0010a3
Johnny <johnny.jarecsni@icloud.com>
2022-03-05 22:11:20 +0000

    Prepare page widget for rendering actual content

--- src/nodeviz/widgets/page/PageRendererComponent.svelte
+++ src/nodeviz/widgets/page/PageRendererComponent.svelte
@@ -1,4 +1,14 @@
 <script type="ts">
-    export let value:unknown;
+    import type { SvelteComponent } from "svelte";
+
+    export let value:{pageComponent:()=>Promise<SvelteComponent>};
+    const pageComponentPromise = value.pageComponent();
 </script>
-<div>Page {JSON.stringify(value)}</div>
+
+{#await pageComponentPromise then pageComponent}
+    {#if pageComponent === undefined} 
+        <p>no page component loaded</p>
+    {:else}
+        <svelte:component this={pageComponent.default}/>
+    {/if}
+{/await}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,9 +4,48 @@
     import GenericComponent from "../nodeviz/GenericComponent.svelte";
     import type { Node } from "src/nodeviz/Nodes";
 
+    const loadPageComponent = (name:string) => import('./resources/' + name + '.svelte');
+ 
+    // 
     const node:Node = {
         type: 'page',
-        value: { some: 'value' }
+        value: { 
+            pageComponent: () => loadPageComponent('HomePage') 
+        },
+        children: [
+            {
+                type: 'page',
+                value: {
+                    pageComponent: () => loadPageComponent('News')
+                }
+            },
+            {
+                type: 'page',
+                value: {
+                    pageComponent: () => loadPageComponent('AboutUs')
+                }
+            },
+            {
+                type: 'page',
+                value: {
+                    pageComponent: () => loadPageComponent('Jobs')
+                },
+                children: [
+                    {
+                        type: 'page',
+                        value: {
+                            pageComponent: () => loadPageComponent('ContractJobs')
+                        }
+                    },
+                    {
+                        type: 'page',
+                        value: {
+                            pageComponent: () => loadPageComponent('Employment')
+                        }
+                    },      
+                ]
+            }
+        ]
     }
 </script>
 
