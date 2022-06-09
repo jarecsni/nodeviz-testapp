74450b99f3f79c4b246da6504366e757cce4bee2
Janos Jarecsni <janos.jarecsni@gmail.com>
2022-06-09 08:47:02 +0100

    PropertyEditor basics and story which doesnt yet work

---
+++ src/nodeviz/common/PropertyEditor.svelte
@@ -0,0 +1,35 @@
+<Accordion multiple>
+    {#each sections as section}
+    <Panel>
+        <Header>{section.name}</Header>
+        <Content>
+          The content for {section.name}.
+        </Content>
+    </Panel>
+    {/each}
+</Accordion>
+
+<script>
+    export let obj;
+    import Accordion, { Panel, Header, Content } from '@smui-extra/accordion'
+    /*
+        example object
+        {
+            sections: [
+                {
+                    name: "First",
+                    properties: {
+
+                    }
+                },
+                {
+                    name: "Second",
+                    properties: {
+
+                    }
+                }
+            ]
+        }
+    */
+   const {sections} = object;
+</script>
---
+++ src/stories/PropertyEditor.stories.svelte
@@ -0,0 +1,40 @@
+<!-- Button.stories.svelte -->
+
+<script>
+    import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+    import PropertyEditor from '../nodeviz/common/PropertyEditor.svelte';
+  </script>
+  
+  <Meta
+    title="Example/PropertyEditor"
+    component={PropertyEditor}
+    argTypes={{
+      obj: {
+        type: {
+          sections: [{type: {
+            name: string
+          }}]
+        }
+      }
+    }}
+  />
+  
+  <!--👇 We create a “template” of how args map to rendering -->
+  
+  <Template let:args>
+    <PropertyEditor {...args} />
+  </Template>
+  
+  <!-- 👇 Each story then reuses that template -->
+  
+  <Story
+    name='Simple'
+    args={{
+      object: {
+        sections: [
+          {name: 'First'},
+          {name: 'Second'}          
+        ]
+      }
+    }}
+  />
