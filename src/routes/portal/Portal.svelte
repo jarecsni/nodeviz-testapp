0e367510217547af81dcb7ba3a9db53af4608c56
Janos Jarecsni <janos.jarecsni@gmail.com>
2022-06-10 08:30:35 +0100

    Fix portal index page

--- src/routes/portal/Portal.svelte
+++ src/routes/portal/Portal.svelte
@@ -1,17 +1,9 @@
 {#if adminMode}
     <Button>Add Widget</Button>
-    <PropertyEditor {obj}></PropertyEditor>
 {/if}
 
 
 <script lang="ts">
 	import Button from '@smui/button';
-    import PropertyEditor from '../../nodeviz/common/PropertyEditor.svelte';
     export let adminMode:boolean = false;
-    const obj = {
-        sections: [
-            {name: 'first'},
-            {name: 'second'}
-        ]
-    }
 </script>
