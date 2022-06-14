ec634a45b9f443ebb1ac1252faca4c08e342a42f
Janos Jarecsni <janos.jarecsni@gmail.com>
2022-06-14 07:52:11 +0100

    Fix ellipse button, add dialogue place holder for string editing

--- src/lib/nodeviz/common/property-editor/editors/StringEditor.svelte
+++ src/lib/nodeviz/common/property-editor/editors/StringEditor.svelte
@@ -3,13 +3,30 @@
         class="material-icons" 
         slot="trailingIcon" 
         size="button"
+        on:click={()=>{open = true;}}
     >
         more_horiz
     </IconButton>
 </Textfield>
+<Dialog
+  bind:open
+>
+  <Title id="editor-title">Edit</Title>
+  <Content id="editor-content">Super awesome dialog body text?</Content>
+  <Actions>
+    <Button on:click={() => {open = false;}}>
+      <Label>Close</Label>
+    </Button>
+  </Actions>
+</Dialog>
 
 <script lang="ts">
 	import Textfield from '@smui/textfield';
     import IconButton from '@smui/icon-button';
+    import Dialog, { Title, Content, Actions } from '@smui/dialog';
+    import Button, { Label } from '@smui/button';
+    import './global.scss';
     export let value;
+    let open;
 </script>
+
